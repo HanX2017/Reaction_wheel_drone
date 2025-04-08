22ba4b0970af8776474dd8d52f91e83243414756
ProgarmerLeo <105867299+ProgarmerLeo@users.noreply.github.com>
2025-04-09 02:10:39 +0800

    Delete copter_4_8/copter_4_8_v2 directory

--- copter_4_8/copter_4_8_v2/python code/find_mac.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bleak import BleakScanner
-
-async def scan():
-    devices = await BleakScanner.discover()
-    for device in devices:
-        print(f"Device found: {device.address} - {device.name}")
-
-asyncio.run(scan())
